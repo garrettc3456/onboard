a8a8b60b2a4c78475499c7abf57a4637b078e101
Garrett Calbert <garrettcalbert@gmail.com>
2014-05-01 08:06:51 -0700

    added signup to the site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+# the application controller manages EVERY controller of the website
+
+# I want there to be a shortcut for finding the current user
+# across the whole site
+# we decide the name, in this case "current_user"
+	def current_user
+		if session[:user_id].present?
+			User.find(session[:user_id])
+		end
+	end
+
+# to make the current user method above useful in the views
+# add a help method
+helper_method :current_user
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501131225) do
+ActiveRecord::Schema.define(version: 20140501135529) do
 
   create_table "rooms", force: true do |t|
     t.string   "name"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20140501131225) do
   end
 
   create_table "roooms", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
